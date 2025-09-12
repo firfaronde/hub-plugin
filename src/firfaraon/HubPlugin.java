74a76dedb5911af3cf0f423175fe274239ddf2e5
firfaronde <223457997+firfaronde@users.noreply.github.com>
2025-09-12 21:48:17 +0500

    upd

--- src/firfaraon/HubPlugin.java
+++ src/firfaraon/HubPlugin.java
@@ -2,15 +2,30 @@ package firfaraon;
 
 import arc.Core;
 import arc.Events;
+import arc.math.geom.Point2;
 import arc.struct.Seq;
 import arc.util.CommandHandler;
 import arc.util.Log;
+import arc.util.Strings;
 import arc.util.Timer;
 import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
 import mindustry.mod.Plugin;
+import mindustry.net.Administration;
 
 public class HubPlugin extends Plugin {
-    public static Seq<Server> servers = new Seq<Server>();
+    public static Seq<Server> servers;
+    // 0 on map, gamemode
+    // 1 on Maze, sandbox
+    public static final String defaultLabel = "[stat]%p on %m[stat], %g";
+    public static Administration.Config labelmessage = new Administration.Config("labelmessage", "Status table message", defaultLabel, null, ()->{
+        String message = Core.settings.getString("labelmessage", defaultLabel);
+        Log.info("Formatted status table @", message.replace("%p", "Player count").replace("%m", "mapname").replace("%g", "mode name"));
+    });
+    public static Administration.Config labelDistance = new Administration.Config("labeldistance", "Distance between labels", 2, null, ()->{});
+    public static Administration.Config offlineMessage = new Administration.Config("offlinemessage", "This message displays when server offline", "[scarlet]Offline", null, ()->{});
+    public static Administration.Config connectdst = new Administration.Config("connectdst", "Distance at which plugin redirects player to server", 2, null, ()->{});
 
     @Override
     public void init() {
@@ -33,6 +48,7 @@ public class HubPlugin extends Plugin {
                         Integer.parseInt(args[4])
                 ));
                 save();
+                Log.info("Server added!");
             } catch (NumberFormatException e) {
                 Log.err("Invalid x, y or port!", e);
             }
@@ -51,15 +67,64 @@ public class HubPlugin extends Plugin {
             save();
             Log.info("Servers saved!");
         });
+
+        handler.register("remove-server", "<id>", "Remove server from list", (args)->{
+            if(!Strings.canParseInt(args[0])) {
+                Log.info("Cannot parse @", args[0]);
+                return;
+            }
+
+            Server s = servers.find(ss->ss.id==Integer.parseInt(args[0]));
+
+            servers.remove(s);
+
+            Log.info("Removed [@] @", s.id, s.name);
+            save();
+        });
+
+        handler.register("rename-server", "<id> <name>", "Re-name server in the list", (args)->{
+            if(!Strings.canParseInt(args[0])) {
+                Log.info("Cannot parse @", args[0]);
+                return;
+            }
+
+            Server s = servers.find(ss->ss.id==Integer.parseInt(args[0]));
+            String from = s.name;
+
+            s.name = args[1];
+
+            Log.info("Re-named from @ to @", from, s.name);
+            save();
+        });
     }
 
     public static void loadHub() {
-        Core.settings.getJson("servers", Seq.class, Seq::new);
+        // servers = Core.settings.getJson("servers", Seq.class, Seq::new);
+	    servers = Core.settings.getJson("servers", Seq.class, Server.class, Seq::new);
+        Log.info("Loaded @ servers total", servers.size);
         Timer.schedule(()->{
             for(Server server : servers) {
                 server.label();
             }
         }, 0, 3);
+
+        Timer.schedule(() -> {
+            Groups.player.each(p -> {
+                servers.each(s -> {
+                    if(!s.offline) {
+                        int px = (int) (p.x / 8);
+                        int py = (int) (p.y / 8);
+                        int sx = Point2.x(s.pos);
+                        int sy = Point2.y(s.pos);
+
+                        float distance = (float) Math.sqrt((sx - px) * (sx - px) + (sy - py) * (sy - py));
+                        if (distance <= Core.settings.getInt("connectdst", 2)) {
+                            Call.connect(p.con, s.ip, s.port);
+                        }
+                    }
+                });
+            });
+        }, 0, 1);
     }
 
     public int getNextServerId() {
@@ -71,4 +136,5 @@ public class HubPlugin extends Plugin {
         Core.settings.putJson("servers", Server.class, servers);
         Log.debug("Servers saved!");
     }
+
 }
--- src/firfaraon/Server.java
+++ src/firfaraon/Server.java
@@ -1,25 +1,35 @@
 package firfaraon;
 
+import arc.Core;
 import arc.math.geom.Point2;
 import arc.scene.event.SceneEvent;
 import arc.util.Log;
 import mindustry.Vars;
 import mindustry.gen.Call;
 
+import static firfaraon.HubPlugin.defaultLabel;
+
 public class Server {
-    public final String name;
+    public String name;
     public final int id;
 
     public final String ip;
     public final int port;
 
     public final int pos;
-    public final float labelSize;
-    public final int labelDistance = 2;
+
+    public transient boolean offline = false;
+
+    public Server(){
+        this.name = "";
+        this.id = -1;
+        this.ip = "";
+        this.port = 0;
+        this.pos = 0;
+    }
 
     public Server(String name, int id, int x, int y, String ip, int port){
         this.pos = Point2.pack(x, y);
-        labelSize = 1f;
         this.name = name;
         this.id = id;
         this.ip = ip;
@@ -30,11 +40,27 @@ public class Server {
      * Spawns label at pos
      * */
     public void label() {
+        int labelDistance = Core.settings.getInt("labeldistance", 2);
         Vars.net.pingHost(ip, port, s->{
-            Call.label(s.name, 3.2f, Point2.x(pos), Point2.y(pos));
-            Call.label(s.players + " players on "+s.mapname+", "+s.modeName, 3.2f, Point2.x(pos), Point2.y(pos));
+            Call.label(s.name, 3.5f, Point2.x(pos)*8, (Point2.y(pos)+labelDistance)*8);
+            String message = Core.settings.getString("labelmessage", defaultLabel);
+            String modeName;
+            if(s.modeName == null)
+                modeName = s.mode.name();
+            else
+                modeName = s.modeName;
+            message = message.replace("%p", s.players+"").replace("%m", s.mapname).replace("%g", modeName);
+            Call.label(message, 3.55f, Point2.x(pos)*8, (Point2.y(pos)-labelDistance)*8);
+            setOffline(false);
         }, f->{
+            Call.label(Core.settings.getString("offlinemessage", "[scarlet]Offline"), 3.55f, Point2.x(pos)*8, (Point2.y(pos)-labelDistance)*8);
             Log.debug("Failed to ping host!", f);
+	        Log.debug(f);
+            setOffline(true);
         });
+    }
+
+    public void setOffline(boolean o) {
+        this.offline=o;
     }
 }
